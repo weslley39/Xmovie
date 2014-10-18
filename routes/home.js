3961a0fdf56106d7adbec7f15658971d3674d5c1
Weslley Neri <weslley39@gmail.com>
2014-10-17 22:38:15 -0300

    View terminada

--- routes/home.js
+++ routes/home.js
@@ -21,34 +21,41 @@ module.exports = exports = function(app, db){
         var cb = function (movies) {
             LastUpdate.findOne(function (err, data) {
                 if(err) throw err;
-
                 var date = new Date();
-                var daysPassed = Math.abs(date.getTime() - new Date(data.LastDate).getTime());
-                daysPassed = Math.floor(daysPassed / (1000 * 3600 * 24)); 
+                var daysPassed;
 
-                //TODO - Melhorar o esquema de atualização
-                if(daysPassed >= 7){
-                    var today = {
-                        'LastDate': date,
-                        'DayOfWeek': date.getDay()
-                    };
-                    console.log(today);
-                    LastUpdate.create(today, function (err, data) {
-
-                    })
+                if (data) {
+                    daysPassed = Math.abs(date.getTime() - new Date(data.LastDate).getTime());
+                    daysPassed = Math.floor(daysPassed / (1000 * 3600 * 24)); 
+                } else {
+                    daysPassed = 8;
                 }
+                    //TODO - Melhorar o esquema de atualização
+                    if(daysPassed >= 7){
+                        var today = {
+                            'LastDate': date,
+                            'DayOfWeek': date.getDay()
+                        };
+                        LastUpdate.create(today, function (err, data) {
 
-                Movie.remove({}, function(err) { 
-                    console.log('collection removed');
-                    Movie.create(movies, function (err, movies) {
-                        console.log(movies);
-                        if (err) throw err;
+                        })
+                    } else {
+
+                    }
+
+
+
+                    Movie.remove({}, function(err) { 
+                        console.log('collection removed');
+                        Movie.create(movies, function (err, movies) {
+                            console.log(movies);
+                            if (err) throw err;
+                        });
+                        res.send('Check your console!')
                     });
-                    res.send('Check your console!')
+
+
                 });
-
-
-            });
 };
 crawler.updateListMovies(cb);
 });
