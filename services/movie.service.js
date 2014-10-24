ac83621266e5e4a2e0e7fd5314936b55869bcc47
Weslley Neri <weslley39@gmail.com>
2014-10-24 21:49:15 -0200

    refactoring the functions for list and update the movies

--- services/movie.service.js
+++ services/movie.service.js
@@ -1,7 +1,15 @@
+var crawler = require('../components/crawler');
 var Movie = require('../schemas/movie');
+var LastUpdate = require('../schemas/lastUpdate');
+var movieService = require('../services/movie.service.js');
+
 
 module.exports = {
-	getMovies: function (cb) {
+	getMovies: getMovies
+};
+
+function getMovies (cb) {
+	updateMovies(function() {
 		Movie.find(function (err, movies) {
 			var mapDay = {1: 'Segunda', 2: 'Terca', 3: 'Quarta', 4: 'Quinta', 5: 'Sexta', 6: 'Sabado', 0: 'Domingo'};
 
@@ -10,7 +18,7 @@ module.exports = {
 			var hour = d.getHours();
 
 			movies.forEach(function(movie, index){
-				var atualDay = movie['Horarios'][mapDay[day]];
+				var atualDay = movie.Horarios[mapDay[day]];
 
 				var atualHours = atualDay.split(" - ");
 				var availableHours = [];
@@ -19,14 +27,49 @@ module.exports = {
 						availableHours.push(' ' + data);
 					}
 				});
-				movies[index]['Horarios'] = '';
+				movies[index].Horarios = '';
 				if (availableHours.length === 0 ) {
-					movies[index]['Horarios'] = ["Sessão Encerrada"];
+					movies[index].Horarios = ["Sessão Encerrada"];
 				} else {
-					movies[index]['Horarios'] = availableHours;
+					movies[index].Horarios = availableHours;
 				}
 			});
 			cb(movies);
 		});
-	}
+	});
+}
+
+function updateMovies (cb) {
+	LastUpdate.findOne(function (err, data) {
+		if(err) throw err;
+		var date = new Date();
+		var daysPassed;
+
+		if (data) {
+			daysPassed = Math.abs(date.getTime() - new Date(data.LastDate).getTime());
+			daysPassed = Math.floor(daysPassed / (1000 * 3600 * 24)); 
+		} else {
+			daysPassed = 8;
+		}
+		if(daysPassed >= 7){
+			var today = {
+				'LastDate': date,
+				'DayOfWeek': date.getDay()
+			};
+			LastUpdate.create(today, function (err, data) {
+				Movie.remove({}, function(err) { 
+					if (err) throw err;
+					crawler.updateListMovies(function(movies) {
+						Movie.create(movies, function (err, movies) {
+							if (err) throw err;
+							cb();
+						});
+					});
+				});
+			});
+		} else {
+			cb();
+		}
+
+	});
 }
