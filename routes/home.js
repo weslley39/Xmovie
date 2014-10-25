f96bc36db55172ceae6e56607aff112953e830b1
Weslley Neri <weslley39@gmail.com>
2014-10-24 22:17:14 -0200

    trying to fix onload movie detail focus in the beginning of the page

--- routes/home.js
+++ routes/home.js
@@ -1,15 +1,18 @@
-var movieService = require('../services/movie.service.js');
+(function() {
+    'use strict';
+    var movieService = require('../services/movie.service.js');
 
-module.exports = exports = function(app, db){
-    app.get('/loadData', function(req, res){
-        var cb = function(movies) {
-            res.contentType('json');
-            res.send(movies);
-        };
-        movieService.getMovies(cb);
-    });
+    module.exports = exports = function(app, db){
+        app.get('/loadData', function(req, res){
+            var cb = function(movies) {
+                res.contentType('json');
+                res.send(movies);
+            };
+            movieService.getMovies(cb);
+        });
 
-    app.get('/', function(req, res, next){
-        return res.render('index');
-    });
-};
+        app.get('/', function(req, res, next){
+            return res.render('index');
+        });
+    };
+})();
