b0e91f45dc6e3e51a6baf0d09606d6b48fdf1023
Weslley Neri <weslley39@gmail.com>
2014-11-17 12:20:22 -0200

    img added and route fixed

--- app/controller.js
+++ app/controller.js
@@ -1,8 +1,8 @@
 (function() {
 	'use strict';
-	angular.module('myApp').controller('MoviesController', ['$scope', '$rootScope', 'MainService', MoviesController]);
+	angular.module('myApp').controller('MoviesController', ['$scope','$location', '$rootScope', 'MainService', MoviesController]);
 
-	function MoviesController ($scope, $rootScope, MainService) {
+	function MoviesController ($scope,$location, $rootScope, MainService) {
 		// $scope.movieDetail = {"Titulo":"Os Boxtrolls (Dub) (3D)","Url":"cinema_lazer.php?id_filme=78","Descricao":"Ovo (Isaac Hempstead-Wright) é um garoto órfão, que desde bebê foi criado nos esgotos da cidade de Ponte Queijo pelos boxtrolls, amáveis criaturas que vivem do lixo deixado por humanos. Como os boxtrolls são caçados impiedosamente por Archibald Snatcher (Ben Kingsley) e sua gangue, eles apenas deixam o subterrâneo à noite e, ainda assim, de vez em quando um deles é capturado. \r\n\r\nQuando Peixe, o boxtrolls que criou Eggs, é pego, o garoto decide se aventurar pela cidade para resgatá-lo. É quando conhece Winnie (Elle Fanning), uma garota mimada que faz com que perceba que ele é, na verdade, um humano.","Img":"http://www.shoppingpatioguarulhos.com.br/banco_imagens/filmes/filme_78.jpg","DataInicial":"02/10/2014","DataFinal":"17/10/2014","Genero":"Aventura , Família , Animação , Fantasia","Censura":"Livre","Elenco":"Isaac Hempstead-Wright, Elle Fanning, Ben Kingsley","Diretor":"Graham Annable, Anthony Stacchi","_id":"543888c63e447a470c616d31","__v":0,"Horarios":"","$$hashKey":"00H"};
 		
 		function init () {
@@ -29,6 +29,14 @@
 		$scope.showMovieDetail = function (movieIndex) {
 			$rootScope.movieDetail = $scope.moviesList[movieIndex];
 			// var local = JSON.parse(localStorage.getItem('movies'));
+		};
+
+		$scope.goPrices = function() {
+			$location.path("/prices");
+		};
+
+		$scope.goHome = function() {
+			$location.path("/");
 		};
 
 		init();
